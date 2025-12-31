91e86da7c7e8807069a3b3c69cb7274fa977c543
trailblaze78 <jerry.co45@tutamail.com>
2025-12-31 21:13:42 +0100

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+ 
+ #validation block
+if [ "$EUID" -ne 0 ]; then
+    echo "Error: This installer needs to be run as root."
+    exit1
+fi
+
+if ! command -v systemctl >/dev/null 2>&1; then
+    echo "Error: systemd is not available on this system."
+    exit1
+fi
+
+if ! command -v nmcli >/dev/null 2>&1; then
+    echo "Error: NetworkManager (nmcli) is not available on this system."
+    exit1
+fi
+
+if ! command systemctl is-active NetworkManager >/dev/null 2>&1; then
+    echo "Error: NetworkManager is not running."
+    exit1
+fi
+
+ #installation block
+SERVICE_SRC="./system/vpntime.service"
+SERVICE_DST="/etc/systemd/system/vpntime.service"
+install -m 644 "$SERVICE_SRC" "$SERVICE_DST"
+systemctl daemon-reload
+systemctl enable vpntime.service
