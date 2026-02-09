088c851204dc1a0dd4b5e9456b48d2b6808ed63f
trailblaze78 <jerry.co45@tutamail.com>
2026-02-09 17:00:14 +0000

    Update vpntime.sh

--- Linux/system/vpntime.sh
+++ Linux/system/vpntime.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
     sleep 2;
+
+    . /etc/vpn-tz.conf
+    FALLBACK_TZ="${DEFAULT_TZ}"
+    
     CODE=$(curl -o /dev/null -fs --max-time 5 -w "%{http_code}\n" https://www.google.com);
     TZ=$(curl -s "https://ipwho.is/?fields=timezone" | grep -oP '"id":\s*"\K[^"]+');
 
@@ -7,5 +11,5 @@
     if [ "$CODE" = "200" ] && [ -n "$TZ" ]; then
         timedatectl set-timezone "$TZ";
     else
-        timedatectl set-timezone $TZ;
+        timedatectl set-timezone "$FALLBACK_TZ"
     fi
